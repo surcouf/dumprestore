8dbc9883697afaecbcf8f03cda61e2269de3e0c8
Raphaël 'SurcouF' Bordet <surcouf@debianfr.net>
2016-04-28 01:42:57 +0200

    Bash formatting.

--- backup_system.sh
+++ backup_system.sh
@@ -55,10 +55,10 @@ clean_exit() {
   find /var/log -name "backup_*.out" -mtime +90 -exec rm {} \;
   chown ftpperf:ftpusers /var/log/backup*out
   chmod 644 /var/log/backup*out
-  cp /var/log/backup*out /mnt/nfsbackup/$HOST 2>/dev/null
+  cp /var/log/backup*out /mnt/nfsbackup/${HOST} 2>/dev/null
   echo -e "\033[40;1;32m=== DEMONTAGE DU PARTAGE NFS ===\033[0m\n"
   umount -f /mnt/nfsbackup 2>/dev/null
-  exit $RETURN
+  exit ${RETURN}
 }
 
 trap clean_exit SIGHUP SIGINT SIGTERM
@@ -67,8 +67,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/seos/bin:
 export PATH
 
 # Verif root
-if [ $EUID -ne 0 ]
-  then
+if [[ ${EUID} -ne 0 ]]; then
   echo "Ce script doit etre lance en tant que root" 1>&2
   exit 1
 fi
@@ -97,205 +96,204 @@ case "${RHEL_VERSION}" in
 esac
 
 # Releve de la config Hardware du serveur physique
-if [ -x /root/hardware.sh -a -x /sbin/hpasmcli -a -x /sbin/hplog -a -x /opt/compaq/hpacucli/bld/hpacucli ]
-  then
+if [ -x /root/hardware.sh -a -x /sbin/hpasmcli -a -x /sbin/hplog -a -x /opt/compaq/hpacucli/bld/hpacucli ]; then
   /root/hardware.sh show
-elif [ -x /root/hardware.sh -a -x /opt/dell/srvadmin/bin/omreport ]
-  then
+elif [ -x /root/hardware.sh -a -x /opt/dell/srvadmin/bin/omreport ]; then
   /root/hardware.sh show
 fi
 
 {
-START=$(date +%s)
-exec 2>&1
-CORE=$(grep -c 'processor' /proc/cpuinfo)
-export CORE
+  exec 2>&1
+  START=$(date +%s)
+  CORE=$(grep -c 'processor' /proc/cpuinfo)
+  export CORE
 
-# fsarchiver ne supporte que jusqu'a 32 jobs simulatnes. On limite donc CORE a 32.
-if [ $CORE -gt 32 ]
-  then
-  export CORE=32
-fi
+  # fsarchiver ne supporte que jusqu'a 32 jobs simulatnes. On limite donc CORE a 32.
+  if [[ ${CORE} -gt 32 ]]; then
+    export CORE=32
+  fi
 
-echo -e "\033[40;1;32m=== SAUVEGARDE SYSTEME rh$RHEL_VERSION ===\033[0m"
+  echo -e "\033[40;1;32m=== SAUVEGARDE SYSTEME rh${RHEL_VERSION} ===\033[0m"
 
-echo -e "\033[43;6;30m=== Log de l'execution du script $DIRLOG/$LOGFILE ===\033[0m"
-echo "=> Debut ${DATE}"
+  echo -e "\033[43;6;30m=== Log de l'execution du script ${DIRLOG}/${LOGFILE} ===\033[0m"
+  echo "=> Debut ${DATE}"
 
-echo -e "\033[40;1;32m=== SUPPRESSION DE SNAPSHOT EVENTUELLEMENT EXISTANT ===\033[0m"
-if lvscan | grep snap | egrep 'slash|usr|opt|var|seos|home'
-  then
-  case $RHEL_VERSION in
-    5) lvremove -f /dev/$(grep usr /proc/mounts | head -n1 | cut -d/ -f3)/{slash,usr,opt,var,seos,home}*snap ;;
-    6) lvremove -f $(grep usr /proc/mounts | head -n1 | cut -d- -f1)-{slash,usr,opt,var,seos,home}*snap ;;
+  echo -e "\033[40;1;32m=== SUPPRESSION DE SNAPSHOT EVENTUELLEMENT EXISTANT ===\033[0m"
+  lvscan | grep snap | egrep 'slash|usr|opt|var|seos|home'
+  if [[ $? -eq 0 ]]; then
+    case ${RHEL_VERSION} in
+      5) lvremove -f /dev/$(grep usr /proc/mounts | head -n1 | cut -d/ -f3)/{slash,usr,opt,var,seos,home}*snap ;;
+      6) lvremove -f $(grep usr /proc/mounts | head -n1 | cut -d- -f1)-{slash,usr,opt,var,seos,home}*snap ;;
+      *)
+        echo "Erreur : OS Inconnu"
+        clean_exit ;;
+     esac
+  else
+     echo "=> Aucun snapshot systeme detecte"
+  fi
+
+  echo -e "\033[40;1;32m=== SUPPRESSION de backupLV - liberation des 6G reserves pour les snapshots ===\033[0m"
+  lvscan | grep -q backupLV
+  if [[ $? -eq 0 ]]; then
+    case ${RHEL_VERSION} in
+      5) lvremove -f /dev/$(grep usr /proc/mounts | head -n1 | cut -d/ -f3)/backupLV ;;
+      6) lvremove -f $(grep usr /proc/mounts | head -n1 | cut -d- -f1)/backupLV ;;
+      *)
+        echo "Erreur : OS Inconnu"
+        clean_exit ;;
+    esac
+  else
+    echo "=> backupLV n'existe pas"
+  fi
+
+  echo -e "\033[40;1;32m=== MONTAGE DU PARTAGE NFS POUR DEPOT DE LA SAUVEGARDE ===\033[0m"
+
+  if [[ -d "/mnt/nfsbackup" ]]; then
+    echo "=> Le repertoire /mnt/nfsbackup existe "
+  else
+    echo "=> Creation du repertoire /mnt/nfsbackup"
+    mkdir -p /mnt/nfsbackup
+  fi
+
+  grep -q "nfsbackup nfs" /etc/mtab
+  if [[ $? -eq 0 ]]; then
+    echo "=> Partage NFS deja monte"
+  else
+    echo "=> Montage du partage NFS"
+    mount -t nfs4 r-isis.unix.intra.bdf.local:/home/svsys/appli/bdf/nfsbackup /mnt/nfsbackup
+    if [[ $? -ne 0 ]]; then
+      echo "Erreur : Montage NFS"
+      clean_exit
+    fi
+  fi
+
+  if [[ -d "/mnt/nfsbackup/${HOST}" ]]; then
+    echo "=> Le repertoire /mnt/nfsbackup/${HOST} existe"
+  else
+    echo "=> Creation du repertoire qui va accueillir la sauvegarde du serveur"
+    mkdir /mnt/nfsbackup/${HOST}
+  fi
+
+  NFS_FREE=$(df -Pk /mnt/nfsbackup | tail -1 | awk '{print $4}')
+  if [[ ${NFS_FREE} -lt 10485760 ]]; then
+    echo "Erreur : Place libre insuffisante sur le partage NFS"
+    clean_exit
+  fi
+
+  FSARCHIVER=$(which fsarchiver)
+  if [[ $? -eq 1 ]]; then
+    case ${RHEL_VERSION} in
+      5)
+          echo "=> fsarchiver introuvable, recuperation du binaire"
+          cp /mnt/nfsbackup/fsarchiver-el5/fsarchiver /usr/local/sbin/
+          FSARCHIVER="/usr/local/sbin/fsarchiver"
+          chmod ug+x ${FSARCHIVER}
+      ;;
+      6)
+          yum -q -y install fsarchiver
+          FSARCHIVER=$(which fsarchiver)
+      ;;
+    esac
+  fi
+
+  cd /mnt/nfsbackup/${HOST}
+
+  case ${RHEL_VERSION} in
+    5) lvdisplay | egrep -i 'slashlv|usrlv|optlv|varlv|seoslv|homelv' | awk '{ print $3 }' | sort > lvm.out ;;
+    6) lvdisplay | grep -i "LV Path" | egrep -i 'slashlv|usrlv|optlv|varlv|seoslv|homelv' | awk '{ print $3 }' | sort > lvm.out ;;
     *)
       echo "Erreur : OS Inconnu"
       clean_exit ;;
-   esac
-else
-   echo "=> Aucun snapshot systeme detecte"
-fi
-
-echo -e "\033[40;1;32m=== SUPPRESSION de backupLV - liberation des 6G reserves pour les snapshots ===\033[0m"
-lvscan | grep -q backupLV || echo "backupLV n'existe pas"
-case $RHEL_VERSION in
-  5) lvscan | grep -q backupLV && lvremove -f /dev/$(grep usr /proc/mounts | head -n1 | cut -d/ -f3)/backupLV ;;
-  6) lvscan | grep -q backupLV && lvremove -f $(grep usr /proc/mounts | head -n1 | cut -d- -f1)/backupLV ;;
-  *)
-    echo "Erreur : OS Inconnu"
-    clean_exit ;;
-esac
-
-echo -e "\033[40;1;32m=== MONTAGE DU PARTAGE NFS POUR DEPOT DE LA SAUVEGARDE ===\033[0m"
-
-if [ -d "/mnt/nfsbackup" ]
-  then
-  echo "=> Le repertoire /mnt/nfsbackup existe "
-else
-  echo "=> Creation du repertoire /mnt/nfsbackup"
-  mkdir -p /mnt/nfsbackup
-fi
-
-if grep -q "nfsbackup nfs" /etc/mtab
-  then
-  echo "=> Partage NFS deja monte"
-else
-  echo "=> Montage du partage NFS"
-  mount -t nfs4 r-isis.unix.intra.bdf.local:/home/svsys/appli/bdf/nfsbackup /mnt/nfsbackup
-  if [ $? -ne 0 ]
-    then
-    echo "Erreur : Montage NFS"
-    clean_exit
-  fi
-fi
-
-if [ -d "/mnt/nfsbackup/$HOST" ]
-  then
-  echo "=> Le repertoire /mnt/nfsbackup/$HOST existe"
-else
-  echo "=> Creation du repertoire qui va accueillir la sauvegarde du serveur"
-  mkdir /mnt/nfsbackup/$HOST
-fi
-
-if [ `df -Pk /mnt/nfsbackup | tail -1 | awk '{print $4}'` -lt "10485760" ]
-  then
-  echo "Erreur : Place libre insuffisante sur le partage NFS"
-  clean_exit
-fi
-
-FSARCHIVER=$(which fsarchiver)
-if [[ $? -eq 1 ]]; then
-  case $RHEL_VERSION in
-    5)
-        echo "=> fsarchiver introuvable, recuperation du binaire"
-        cp /mnt/nfsbackup/fsarchiver-el5/fsarchiver /usr/local/sbin/
-        FSARCHIVER="/usr/local/sbin/fsarchiver"
-        chmod ug+x ${FSARCHIVER}
-    ;;
-    6)
-        yum -q -y install fsarchiver
-        FSARCHIVER=$(which fsarchiver)
-    ;;
   esac
-fi
 
-cd /mnt/nfsbackup/$HOST
-
-case $RHEL_VERSION in
-  5) lvdisplay | egrep -i 'slashlv|usrlv|optlv|varlv|seoslv|homelv' | awk '{ print $3 }' | sort > lvm.out ;;
-  6) lvdisplay | grep -i "LV Path" | egrep -i 'slashlv|usrlv|optlv|varlv|seoslv|homelv' | awk '{ print $3 }' | sort > lvm.out ;;
-  *)
-    echo "Erreur : OS Inconnu"
-    clean_exit ;;
-esac
-
-IFS=$'\n'
-sync
-
-echo -e "\033[40;1;32m=> Sauvegarde de la partition /boot sous isis:/mnt/nfsbackup/$HOST/boot.fsa\033[0m"
-if [ $(mount |grep -c boot) = 0 ]
-  then
-  echo "Pas de partition /boot separe"
-else
-  BOOT=`mount | grep boot | awk '{print $1}'`
-  mount -o remount,ro $BOOT
+  IFS=$'\n'
   sync
-  grep -q "boot ext2" /etc/mtab && (echo "=> filesystem EXT2 pour boot, ajout argument -a" && /usr/bin/time -f "\n%E elapsed" fsarchiver savefs -o -a -z7 -j$CORE BOOT.fsa $BOOT) || (echo "EXT3 ou EXT4 pour boot" && /usr/bin/time -f "\n%E elapsed" fsarchiver savefs -o -z7 -j$CORE BOOT.fsa $BOOT)
-  mount -o remount,rw $BOOT
+
+  echo -e "\033[40;1;32m=> Sauvegarde de la partition /boot sous isis:/mnt/nfsbackup/${HOST}/boot.fsa\033[0m"
+  mount | grep -q boot
+  if [[ $? -eq 0 ]]; then
+    echo "Pas de partition /boot separe"
+  else
+    BOOT=$(mount | grep boot | awk '{print $1}')
+    mount -o remount,ro ${BOOT}
+    sync
+    grep -q "boot ext2" /etc/mtab
+    if [[ $? -eq 0 ]]; then
+      echo "=> filesystem EXT2 pour boot, ajout argument -a"
+      /usr/bin/time -f "\n%E elapsed" fsarchiver savefs -o -a -z7 -j${CORE} BOOT.fsa ${BOOT}
+    else
+      echo "EXT3 ou EXT4 pour boot"
+      /usr/bin/time -f "\n%E elapsed" fsarchiver savefs -o -z7 -j${CORE} BOOT.fsa ${BOOT}
+    fi
+    mount -o remount,rw ${BOOT}
+    sync
+  fi
+
+  echo -e "\033[40;1;32m=== CREATION DES SNAPSHOTS DES LV SYSTEME ===\033[0m"
+  for lv in $(cat lvm.out); do
+    lvcreate -L1G -s -n `echo ${lv} | cut -d/ -f 4`snap `echo ${lv}`
+    if [[ $? -ne 0 ]]; then
+      echo "Erreur : Creation du snapshot "`echo ${lv} | cut -d/ -f 4`snap `echo ${lv}`
+      clean_exit
+    fi
+  done
+
+  echo -e "\033[40;1;32m=== SAUVEGARDE DES SNAPSHOTS ===\033[0m\n"
+  for lv in $(cat lvm.out); do
+    echo -e "\033[40;1;32m=> ${lv} sauvegarde sous isis:/mnt/nfsbackup/${HOST}/`echo ${lv} | cut -d/ -f 4`.fsa \033[0m"
+    /usr/bin/time -f "\n%E elapsed" fsarchiver savefs -o -z7 -j${CORE} `echo ${lv} | cut -d/ -f 4`.fsa `echo ${lv}`snap
+    if [[ $? -ne 0 ]]; then
+      echo "Erreur : fsarchiver "`echo ${lv}`snap
+      clean_exit
+    fi
+    echo "----------------------------------------------"
+    sleep 2
+  done
+
+  echo -e "\033[40;1;32m=== SUPPRESSION DES SNAPSHOTS ===\033[0m"
+  lvremove -f /dev/*/{slash,usr,opt,var,seos,home}*snap
+
   sync
-fi
 
-echo -e "\033[40;1;32m=== CREATION DES SNAPSHOTS DES LV SYSTEME ===\033[0m"
-for lv in $(cat lvm.out)
-  do
-  lvcreate -L1G -s -n `echo $lv | cut -d/ -f 4`snap `echo $lv`
-  if [ $? -ne 0 ]
-    then
-    echo "Erreur : Creation du snapshot "`echo $lv | cut -d/ -f 4`snap `echo $lv`
-    clean_exit
+  if [ ! -s ks-${HOST}.cfg -a -s /root/*ks.cfg ]; then
+    echo "=> Recuperation du kickstart "
+    cp /root/anaconda-ks.cfg /mnt/nfsbackup/${HOST}/ks-${HOST}.cfg
+  elif [ ! -s ks-${HOST}.cfg -a -s /root/log_install/*ks.cfg ]; then
+    echo "=> Recuperation du kickstart "
+    cp /root/log_install/anaconda-ks.cfg /mnt/nfsbackup/${HOST}/ks-${HOST}.cfg
+  else
+    echo "=> Fichier kickstart deja recupere lors d'une precedente sauvegarde"
   fi
-done
 
-echo -e "\033[40;1;32m=== SAUVEGARDE DES SNAPSHOTS ===\033[0m\n"
-for lv in $(cat lvm.out)
-  do
-  echo -e "\033[40;1;32m=> $lv sauvegarde sous isis:/mnt/nfsbackup/$HOST/`echo $lv | cut -d/ -f 4`.fsa \033[0m"
-  /usr/bin/time -f "\n%E elapsed" fsarchiver savefs -o -z7 -j$CORE `echo $lv | cut -d/ -f 4`.fsa `echo $lv`snap
-  if [ $? -ne 0 ]
-    then
-    echo "Erreur : fsarchiver "`echo $lv`snap
-    clean_exit
-  fi
-  echo "----------------------------------------------"
-  sleep 2
-done
+  echo -e "\033[40;1;32m=== SAUVEGARDE DU MBR ET DE LA TABLE DES PARTITIONS ===\033[0m\n"
+  BOOT_PART=$(cat /proc/partitions |awk '{print $4}' |grep -v "name"|grep -v "loop0" | grep -v '^$' | sed -n 1p)
+  echo ${BOOT_PART}
+  dd if=/dev/${BOOT_PART} of=/mnt/nfsbackup/${HOST}/${HOST}.mbr.backup bs=512 count=1
+  sfdisk -d /dev/${BOOT_PART} > /mnt/nfsbackup/${HOST}/${HOST}.partitions-table.backup
 
-echo -e "\033[40;1;32m=== SUPPRESSION DES SNAPSHOTS ===\033[0m"
-lvremove -f /dev/*/{slash,usr,opt,var,seos,home}*snap
+  cd /
+  SIZE=$(du -sh /mnt/nfsbackup/${HOST} | cut -f 1)
+  echo -e "\033[40;1;32m=== TAILLE DE LA SAUVEGARDE = $SIZE \033[0m"
+  ls -lh /mnt/nfsbackup/${HOST}/*
 
-sync
+  END=$(date +%s)
+  DIFF=$(( ${END} - ${START} ))
+  echo -e "\033[40;1;32m=== DUREE DE LA SAUVEGARDE = $((${DIFF}/60/60)) heure $((${DIFF}/60%60 )) min $((${DIFF}%60)) sec \033[0m"
 
-if [ ! -s ks-$HOST.cfg -a -s /root/*ks.cfg ]
-  then
-  echo "=> Recuperation du kickstart "
-  cp /root/anaconda-ks.cfg /mnt/nfsbackup/$HOST/ks-$HOST.cfg
-elif [ ! -s ks-$HOST.cfg -a -s /root/log_install/*ks.cfg ]
-  then
-  echo "=> Recuperation du kickstart "
-  cp /root/log_install/anaconda-ks.cfg /mnt/nfsbackup/$HOST/ks-$HOST.cfg
-else
-  echo "=> Fichier kickstart deja recupere lors d'une precedente sauvegarde"
-fi
+  DATE=`date +%Y_%m_%d-%H:%M`
+  echo "=> Fin ${DATE}"
 
-echo -e "\033[40;1;32m=== SAUVEGARDE DU MBR ET DE LA TABLE DES PARTITIONS ===\033[0m\n"
-BOOT_PART=$(cat /proc/partitions |awk '{print $4}' |grep -v "name"|grep -v "loop0" | grep -v '^$' | sed -n 1p)
-echo $BOOT_PART
-dd if=/dev/$BOOT_PART of=/mnt/nfsbackup/$HOST/$HOST.mbr.backup bs=512 count=1
-sfdisk -d /dev/$BOOT_PART > /mnt/nfsbackup/$HOST/$HOST.partitions-table.backup
+  echo -e "\033[40;1;32m=== CREATION DE backupLV POUR RESERVATION DES 6G NECESSAIRES AUX SNAPSHOTS ===\033[0m"
 
-cd /
-SIZE=$(du -sh /mnt/nfsbackup/$HOST | cut -f 1)
-echo -e "\033[40;1;32m=== TAILLE DE LA SAUVEGARDE = $SIZE \033[0m"
-ls -lh /mnt/nfsbackup/$HOST/*
+  case "${RHEL_VERSION}" in
+    5) lvcreate -L6144M -n backupLV $(grep usr /proc/mounts | head -n1 | cut -d/ -f3) ;;
+    6) lvcreate -L6144M -n backupLV $(grep usr /proc/mounts | head -n1 | cut -d- -f1) ;;
+    *)
+      echo "Erreur : OS Inconnu"
+      clean_exit ;;
+  esac
 
-END=$(date +%s)
-DIFF=$(( $END - $START ))
-echo -e "\033[40;1;32m=== DUREE DE LA SAUVEGARDE = $(($DIFF/60/60)) heure $(($DIFF/60%60 )) min $(($DIFF%60)) sec \033[0m"
-
-DATE=`date +%Y_%m_%d-%H:%M`
-echo "=> Fin ${DATE}"
-
-echo -e "\033[40;1;32m=== CREATION DE backupLV POUR RESERVATION DES 6G NECESSAIRES AUX SNAPSHOTS ===\033[0m"
-
-case $RHEL_VERSION in
-  5) lvcreate -L6144M -n backupLV $(grep usr /proc/mounts | head -n1 | cut -d/ -f3) ;;
-  6) lvcreate -L6144M -n backupLV $(grep usr /proc/mounts | head -n1 | cut -d- -f1) ;;
-  *)
-    echo "Erreur : OS Inconnu"
-    clean_exit ;;
-esac
-
-} | tee $DIRLOG/$LOGFILE
+} | tee ${DIRLOG}/${LOGFILE}
 
 clean_exit 0
 
